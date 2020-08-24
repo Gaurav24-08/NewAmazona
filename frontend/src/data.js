6b68019216a091308a2c5f888702b50a0d65827c
Gaurav24-08 <gaurav24new@gmail.com>
2020-08-24 16:03:47 +0530

    Second Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+export default {
+    products: [
+      {
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 10
+      },
+      {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/d2.jpg',
+        price: 50,
+        brand: 'Louis Phillipe',
+        rating: 4.2,
+        numReviews: 5
+      },
+      {
+        _id: '3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d3.jpg',
+        price: 70,
+        brand: ' Lee Jeans',
+        rating: 4.5,
+        numReviews: 8
+      }, {
+        _id: '4',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/d1.jpg',
+        price: 70,
+        brand: ' Armani Jeans',
+        rating: 4.5,
+        numReviews: 8
+      },
+      {
+        _id: '5',
+        name: 'Leather Shoes',
+        category: 'Shoes',
+        image: '/images/d5.jpg',
+        price: 120,
+        brand: 'Woodland',
+        rating: 4.7,
+        numReviews: 7
+      },
+      {
+        _id: '6',
+        name: 'Sports Pants',
+        category: 'Shoes',
+        image: '/images/d6.jpg',
+        price: 100,
+        brand: ' Nike',
+        rating: 4.3,
+        numReviews: 17
+      },
+      {
+        _id: '7',
+        name: 'Watch for men',
+        category: 'Wathes',
+        image: '/images/d7.jpg',
+        price: 170,
+        brand: ' Omega',
+        rating: 4.6,
+        numReviews: 24
+      },
+      {
+        _id: '8',
+        name: 'Watch for Women',
+        category: 'Watches',
+        image: '/images/d8.jpeg',
+        price: 150,
+        brand: ' Tissot',
+        rating: 4.9,
+        numReviews: 2
+      },
+    ]
+  }
